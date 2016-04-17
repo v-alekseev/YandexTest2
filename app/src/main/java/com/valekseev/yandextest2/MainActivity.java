a874c8345175fbfcc31f69d147e1c5d9f28fecb0
Vitaly Alekseev <valekseev@baccasoft.ru>
2016-04-18 02:16:43 +0300

    List view with image and 2line text

--- app/src/main/java/com/valekseev/yandextest2/MainActivity.java
+++ app/src/main/java/com/valekseev/yandextest2/MainActivity.java
@@ -9,10 +9,19 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<HashMap<String, Object>> mCatList;
+    private static final String TITLE = "catname"; // Верхний текст
+    private static final String DESCRIPTION = "description"; // ниже главного
+    private static final String ICON = "icon";  // будущая картинка
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +39,40 @@ public class MainActivity extends AppCompatActivity {
         };
 
 // используем адаптер данных
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item, catnames);
+      //  ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item, catnames);
+
+        // создаем массив списков
+        mCatList = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, Object> hm;
+
+        hm = new HashMap<>();
+        hm.put(TITLE, "Рыжик"); // Название
+        hm.put(DESCRIPTION, "Рыжий и хитрый"); // Описание
+        hm.put(ICON, R.drawable.common_google_signin_btn_icon_light); // Картинка
+        mCatList.add(hm);
+
+        hm = new HashMap<>();
+        hm.put(TITLE, "Васька");
+        hm.put(DESCRIPTION, "Слушает да ест");
+        hm.put(ICON, R.drawable.common_plus_signin_btn_icon_light_pressed);
+        mCatList.add(hm);
+
+        hm = new HashMap<>();
+        hm.put(TITLE, "Мурзик");
+        hm.put(DESCRIPTION, "Спит и мурлыкает");
+        hm.put(ICON, R.drawable.ic_cast_on_1_light);
+        mCatList.add(hm);
+
+        hm = new HashMap<>();
+        hm.put(TITLE, "Барсик");
+        hm.put(DESCRIPTION, "Болеет за Барселону");
+        hm.put(ICON, R.drawable.common_plus_signin_btn_icon_light_focused);
+        mCatList.add(hm);
+
+        SimpleAdapter adapter = new SimpleAdapter(this, mCatList,
+                R.layout.list_item, new String[]{TITLE, DESCRIPTION, ICON},
+                new int[]{R.id.text1, R.id.text2, R.id.img});
+
 
         listView.setAdapter(adapter);
 
